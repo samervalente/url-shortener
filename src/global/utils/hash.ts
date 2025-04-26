82b8af98a7bc8cd15256964555b2e57b9fc917e2
Samer Valente <samervalente@gmail.com>
2025-04-26 12:41:55 -0300

    chore: remove src

--- src/global/utils/hash.ts
+++
@@ -1,7 +0,0 @@
-import { genSalt, hashSync } from 'bcrypt';
-
-export async function hashStr(toHash: string) {
-  const saltRounds = 12;
-  const salt = await genSalt(saltRounds);
-  return hashSync(toHash, salt);
-}
